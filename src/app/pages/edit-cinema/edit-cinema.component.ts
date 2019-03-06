3eb83af58736aa6dbbf252f4af818ac75b60a99c
Iñigo Gorosabel Fernandez <inigo.gorosabel@gmail.com>
2019-03-06 23:22:29 +0100

    Avanzo con editar cine

--- src/app/pages/edit-cinema/edit-cinema.component.ts
+++ src/app/pages/edit-cinema/edit-cinema.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }             from '@angular/core';
+import { Router, ActivatedRoute, Params} from '@angular/router';
+import { DataShareService }              from '../../services/data-share.service';
+import { CommonService }                 from '../../services/common.service';
+import { DialogService }                 from '../../services/dialog.service';
+import { Cinema }                        from '../../interfaces/interfaces';
 
 @Component({
   selector: 'app-edit-cinema',
@@ -6,6 +11,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./css/edit-cinema.component.css']
 })
 export class EditCinemaComponent implements OnInit {
-	constructor() { }
-	ngOnInit() {}
+  cinemas: Cinema[] = [];
+  selectedCinema: Cinema = null;
+  editSending: boolean = false;
+
+	constructor(private activatedRoute: ActivatedRoute,
+	            private dss: DataShareService,
+	            private router: Router,
+	            private cs: CommonService,
+	            private dialog: DialogService) { }
+
+	ngOnInit() {
+  	  this.cinemas = this.dss.getGlobal('cinemas');
+  	  if (this.cinemas.length==0){
+    	  this.router.navigate(['/cinemas']);
+  	  }
+  	  this.activatedRoute.params.subscribe((params: Params) => {
+      const id: number = params.id;
+      const ind = this.cinemas.findIndex(x => x.id==id);
+      this.selectedCinema = this.cinemas[ind];
+      this.selectedCinema.name = this.cs.urldecode(this.selectedCinema.name);
+    });
+	}
+	
+	doEdit(ev) {
+  	  ev.preventDefault();
+  	  if (this.cinema==''){
+    	  this.dialog.alert({title: 'Error', content: '¡No puedes dejar el nombre en blanco!', ok: 'Continuar'});
+    	  return;
+  	  }
+  	  
+  	  
+	}
+	
+	deleteCinema() {
+  	  this.dialog.confirm({title: 'Borrar cine',
+    	                     content: '¿Estás seguro de querer borrar este cine? También se borrarán todas sus entradas.',
+    	                     ok: 'Continuar',
+    	                     cancel: 'Cancelar'}).subscribe(result => {
+    	  if (result){
+      	  this.as.deleteCinema(this.selectedCinema.id).subscribe(result => {
+        	  if (result.status=='ok'){
+          	  this.dialog.alert({title: 'Cine borrado', content: 'El cine y todas sus entradas han sido borradas.', ok: 'Continuar'}).subscribe(result => {
+            	  this.dss.removeGlobal('cinemas');
+            	  this.router.navigate(['/cinemas']);
+             });
+        	  }
+      	  });
+    	  }
+  	  });
+	}
 }
