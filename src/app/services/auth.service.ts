605f6f2e4dadfea9fe777c7a8ee686486ad631ec
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2020-02-11 17:54:10 +0100

    Corrección para que compile en Angular 9, actualizo angular-jwt 2.1.0 -> 4.0.0

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable }       from '@angular/core';
-import { JwtHelperService } from '@auth0/angular-jwt';
+import { JwtHelperService } from '@auth0/angular-jwt/src/jwthelper.service';
 import { UserService }      from './user.service';
 
 @Injectable()
