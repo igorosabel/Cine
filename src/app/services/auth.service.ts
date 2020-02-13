2ef7233098bd3a299b9627116d38048b8ec6d0d8
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2020-02-13 18:39:53 +0100

    Actualizo Angular CLI (9.0.1 -> 9.0.2), Angular (9.0.0 -> 9.0.1) y una corrección para que compile

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable }       from '@angular/core';
-import { JwtHelperService } from '@auth0/angular-jwt/src/jwthelper.service';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { UserService }      from './user.service';
 
 @Injectable()
