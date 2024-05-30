cc391354c8d36e5193d443c556cf35935d901056
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2024-05-30 08:35:54 +0200

    Adapto aplicación a estructura de Angular 18, limpieza de archivos antiguos y rediseño a Material Design 3

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -7,26 +7,26 @@ import {
   inject,
   signal,
   viewChild,
-} from "@angular/core";
-import { FormsModule } from "@angular/forms";
-import { MatButtonModule } from "@angular/material/button";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
-import { MatListModule } from "@angular/material/list";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { RouterModule } from "@angular/router";
-import { MoviesResult } from "@interfaces/interfaces";
-import Movie from "@model/movie.model";
-import ApiService from "@services/api.service";
-import ClassMapperService from "@services/class-mapper.service";
-import MovieListComponent from "@shared/components/movie-list/movie-list.component";
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { MoviesResult } from '@interfaces/interfaces';
+import Movie from '@model/movie.model';
+import ApiService from '@services/api.service';
+import ClassMapperService from '@services/class-mapper.service';
+import MovieListComponent from '@shared/components/movie-list/movie-list.component';
 
 @Component({
   standalone: true,
-  selector: "app-search",
-  templateUrl: "./search.component.html",
-  styleUrls: ["./search.component.scss"],
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.scss'],
   imports: [
     FormsModule,
     RouterModule,
@@ -43,7 +43,7 @@ export default class SearchComponent implements OnInit {
   private as: ApiService = inject(ApiService);
   private cms: ClassMapperService = inject(ClassMapperService);
 
-  searchBox: Signal<ElementRef> = viewChild.required("searchBox");
+  searchBox: Signal<ElementRef> = viewChild.required('searchBox');
   q: string | null = null;
   movies: WritableSignal<Movie[]> = signal<Movie[]>([]);
   searchTimer: number = -1;
@@ -69,8 +69,10 @@ export default class SearchComponent implements OnInit {
       this.searchMovieStop();
       this.searching.set(true);
       this.as.searchTitles(this.q).subscribe((result: MoviesResult): void => {
+        console.log('result');
         this.searching.set(false);
         this.movies.set(this.cms.getMovies(result.list));
+        console.log(this.movies());
       });
     } else {
       this.movies.set([]);
